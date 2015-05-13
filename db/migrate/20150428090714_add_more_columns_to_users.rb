b99304ffabbb697d30dad21e9f306791388e63e6
pikpok <pikpok@riseup.net>
2015-05-14 00:09:03 +0200

    test migration

--- db/migrate/20150428090714_add_more_columns_to_users.rb
+++ db/migrate/20150428090714_add_more_columns_to_users.rb
@@ -3,6 +3,6 @@ class AddMoreColumnsToUsers < ActiveRecord::Migration
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :telephone, :string
-    add_column :users, :subscription, :date
+    add_column :users, :subscription, :date, null: false, default: Date.today
   end
 end

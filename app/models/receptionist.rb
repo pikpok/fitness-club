bc6118b7906aea33b0a34dfc03cc27ccafa34407
pikpok <pikpok@riseup.net>
2015-05-19 17:15:12 +0200

    models: added validation to Receptionist, cleaned up User

--- app/models/receptionist.rb
+++ app/models/receptionist.rb
@@ -1,5 +1,7 @@
 class Receptionist < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :trackable, :timeoutable, :lockable
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end

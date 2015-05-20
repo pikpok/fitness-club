08461bd3f56fcdd0ffda00cde7ff4e95fb11213c
pikpok <pikpok@riseup.net>
2015-05-20 11:45:30 +0200

    Admin and Receptionist have their nice dashboards too!

--- config/routes.rb
+++ config/routes.rb
@@ -4,70 +4,27 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   namespace :receptionist do
+    root 'dashboard#index'
     get 'dashboard/index'
-    resources :users
+    resources :users do
+      collection do
+        patch 'update_password'
+      end
+    end
   end
 
   namespace :admin do
+    root 'dashboard#index'
     resources :receptionists
     resources :users
   end
 
-  devise_for :admins
-  devise_for :receptionists
-  devise_for :users, controllers: { sessions: 'devise/sessions', :registrations => "users/registrations" }
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  namespace :users do
+    root 'dashboard#index'
+    get 'dashboard/index'
+  end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  devise_for :admin
+  devise_for :receptionist
+  devise_for :user, controllers: { sessions: 'devise/sessions', :registrations => "users/registrations" }
 end

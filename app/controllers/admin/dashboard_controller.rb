c78926a45dfd7d91f0b78a215babc17efe18543b
pikpok <pikpok@riseup.net>
2015-05-26 11:27:31 +0200

    started some cleaning and commenting out code

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,16 +1,24 @@
 class Admin::DashboardController < ApplicationController
+  # make sure that admin is always signed in before accessing dashboard
   before_filter :authenticate_admin!
 
+  # GET /admin/
   def index
   end
 
+  # GET /admin/edit
   def edit
   end
 
+  # Function to update email and password of currently logged in Admin.
+  # It needs current password to update.
+  # If some of parameters fails to update it redirects to index with errors
+  # 
+  # PATCH /admin/update
   def update
     @admin = current_admin
     if @admin.update_with_password(secure_params)
-      sign_in(@admin, :bypass => true)
+      sign_in(@admin, :bypass => true) # doesn't logout Admin when succesfully changed account
       redirect_to admin_root_path, notice: "Your data has been updated"
     else
       redirect_to admin_root_path, alert: @admin.errors.full_messages.to_sentence
@@ -18,7 +26,9 @@ class Admin::DashboardController < ApplicationController
   end
 
   private
-    def secure_params
-      params.require(:admin).permit(:email, :password, :password_confirmation, :current_password)
-    end
+
+  # Define parameters required for Admin to change password to disallow change of any other parameter
+  def secure_params
+    params.require(:admin).permit(:email, :password, :password_confirmation, :current_password)
+  end
 end
--- app/controllers/admin/receptionists_controller.rb
+++ app/controllers/admin/receptionists_controller.rb
@@ -1,42 +1,53 @@
 class Admin::ReceptionistsController < ApplicationController
-    def index
-      @receptionists = Receptionist.all
-    end
+  # make sure that admin is always signed in before accessing ReceptionistsController
+  before_filter :authenticate_admin!
 
-    def new
-      @receptionist = Receptionist.new
-    end
-  
-    def create
-      @receptionist = Receptionist.new(secure_params)
-      if @receptionist.save
-        redirect_to admin_receptionists_path, notice: "Receptionist created"
-      else
-        redirect_to new_admin_receptionist_path, alert: @receptionist.errors.full_messages.to_sentence
-      end
-    end
-  
-    def update
-      @receptionist = Receptionist.find(params[:id])
-      if @receptionist.update_attributes(secure_params)
-        redirect_to admin_receptionists_path, notice: "Receptionist updated"
-      else
-        redirect_to edit_admin_receptionist_path(id: @receptionist.id), alert: @receptionist.errors.full_messages.to_sentence
-      end
-    end
-  
-    def edit
-      @receptionist = Receptionist.find(params[:id])
-    end
-  
-    def destroy
-      receptionist = Receptionist.find(params[:id])
-      receptionist.destroy
-      redirect_to admin_receptionists_path, notice: "Receptionist removed"
-    end
+  # GET /admin/receptionists
+  def index
+    @receptionists = Receptionist.all
+  end
 
-    private
-      def secure_params
-        params.require(:receptionist).permit(:email, :password, :password_confirmation)
-      end
+  # GET /admin/receptionists/new
+  def new
+    @receptionist = Receptionist.new
+  end
+  
+  # POST /admin/receptionists/create
+  def create
+    @receptionist = Receptionist.new(secure_params)
+    if @receptionist.save
+      redirect_to admin_receptionists_path, notice: "Receptionist created"
+    else
+      redirect_to new_admin_receptionist_path, alert: @receptionist.errors.full_messages.to_sentence
+    end
+  end
+
+  # GET /admin/receptionists/{id}/edit
+  def edit
+    @receptionist = Receptionist.find(params[:id])
+  end
+  
+  # PATCH /admin/receptionists/{id}
+  def update
+    @receptionist = Receptionist.find(params[:id])
+    if @receptionist.update_attributes(secure_params)
+      redirect_to admin_receptionists_path, notice: "Receptionist updated"
+    else
+      redirect_to edit_admin_receptionist_path(id: @receptionist.id), alert: @receptionist.errors.full_messages.to_sentence
+    end
+  end
+  
+  # DELETE /admin/receptionists/{id}
+  def destroy
+    receptionist = Receptionist.find(params[:id])
+    receptionist.destroy
+    redirect_to admin_receptionists_path, notice: "Receptionist removed"
+  end
+
+  private
+
+  # Define parameters required for Admin to change password to disallow change of any other parameter
+  def secure_params
+    params.require(:receptionist).permit(:email, :password, :password_confirmation)
+  end
 end

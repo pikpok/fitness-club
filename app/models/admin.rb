797322a4595242760568ba4c8e8b90c130a39dde
pikpok <pikpok@riseup.net>
2015-05-28 00:31:05 +0200

    fix validation of password for Admin and Receptionist they are certainly not always needed (i.e. when changing own password)

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,5 @@
 class Admin < ActiveRecord::Base
   devise :database_authenticatable, :timeoutable, :validatable
 
-  validates :email, uniqueness: true, presence: true 
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true, presence: true
 end
--- app/models/receptionist.rb
+++ app/models/receptionist.rb
@@ -2,6 +2,4 @@ class Receptionist < ActiveRecord::Base
   devise :database_authenticatable, :trackable, :timeoutable, :lockable, :validatable
 
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 end

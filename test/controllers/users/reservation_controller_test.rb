d5b7daf2f671035ae4444e389d54ca6e93099e9c
pikpok <pikpok@riseup.net>
2015-05-28 02:19:13 +0200

    added some Users::ReservationController tests

--- test/controllers/users/reservation_controller_test.rb
+++ test/controllers/users/reservation_controller_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class Users::ReservationControllerTest < ActionController::TestCase
   setup do
     sign_in users(:one)
+    post :create, pool_reservation: {
+      start_time: DateTime.now + 100.minutes,
+      end_time: DateTime.now + 150.minutes
+    }
   end
 
   test "should get reservations" do
@@ -14,5 +18,35 @@ class Users::ReservationControllerTest < ActionController::TestCase
     sign_out users(:one)
     get :index
     assert_response :redirect
+  end
+
+  test "should create new reservation" do
+    assert_difference("PoolReservation.count", +1) do
+      post :create, pool_reservation: {
+        start_time: DateTime.now + 20.minutes,
+        end_time: DateTime.now + 50.minutes
+      }
+    end
+    assert_redirected_to users_reservation_index_path
+    assert_equal "Pool reserved", flash[:notice]
+  end
+
+  test "should not create invalid reservation" do
+    assert_no_difference("PoolReservation.count") do
+      post :create, pool_reservation: {
+        start_time: DateTime.now + 10.minutes,
+        end_time: DateTime.now - 20.minutes
+      }
+    end
+    assert_redirected_to users_reservation_index_path
+    assert_equal "Start time is wrong and End time is wrong", flash[:alert]
+  end
+
+  test "should delete reservation" do
+    assert_difference("PoolReservation.count", -1) do
+      delete :destroy, id: users(:one).pool_reservations.first.id
+    end
+    assert_redirected_to users_reservation_index_path
+    assert_equal "Reservation removed", flash[:notice]
   end
 end
